a2b9e5e946ca030077e78f4b1d9903756a3329ca
Nicholas S. Zivkovic <niki.zivkovic@hotmail.com>
2024-02-17 23:49:09 +0100

    lots of noew assets not yet integrated
    - need to integrat them

--- src/AA_main/main.gd
+++ src/AA_main/main.gd
@@ -1,18 +1,15 @@
 extends Node
 
-@onready var _host_edit = $Control/HBoxContainer/ClientContainer/LineEdit
+@onready var _host_edit = $LineEdit
 
 
-func _on_agent_btn_pressed():
-	print("agent button pressed")
+func _on_start_server_pressed():
+	get_tree().change_scene_to_file("res://src/director/director.tscn")
+
+
+func _on_input_ip_pressed():
 	Global.host_ip = _host_edit.text
 	if _host_edit.text == "":
 		print("can't have empty ip field")
 		#return
 	get_tree().change_scene_to_file("res://src/agent/agent.tscn")
-
-
-func _on_director_btn_pressed():
-	print("server button pressed")
-	get_tree().change_scene_to_file("res://src/director/director.tscn")
-
