6bc81440f154110beb2e8e04154d7c2c7f9a372d
Nicholas S. Zivkovic <niki.zivkovic@hotmail.com>
2024-02-17 10:13:45 +0100

    pin graphics added

--- src/dir_pins/PinScene.gd
+++ src/dir_pins/PinScene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var pinElement = preload("res://src/dir_pins/PinElement.gd")
+var pinElement = preload("res://src/dir_pins/PinElement.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
